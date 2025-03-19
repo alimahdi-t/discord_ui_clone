ede5a0d6621bde3e5e935c32d3a52c9df3615ee4
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-03-19 18:31:08 +0330

    Add color palate

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex text-white h-screen">
-      <div className="bg-gray-800 p-3 space-y-2 overflow-y-scroll scroll-thin">
+    <div className="flex text-gray-100 h-screen">
+      <div className="bg-gray-900 p-3 space-y-2 overflow-y-scroll scroll-thin">
         {[...Array(40)].map((_, i) => (
           <div
             key={i}
@@ -11,17 +11,19 @@ export default function Home() {
           </div>
         ))}
       </div>
-      <div className="bg-gray-700 w-60 flex flex-col">
+      <div className="bg-gray-800 w-60 flex flex-col">
         <div className="px-3 h-12 flex items-center shadow-md shrink-0">
           Tailwind CSS
         </div>
-        <div className="p-3 flex-1 overflow-y-scroll space-y-2 scroll-thin">
+        <div className="text-gray-300 p-3 flex-1 overflow-y-scroll space-y-2 scroll-thin">
+          <p className={"text-white"}>channel (unread)</p>
+          <p className={"text-white"}>channel (unread)</p>
           {[...Array(40)].map((_, i) => (
             <p key={i}>Channels {i}</p>
           ))}
         </div>
       </div>
-      <div className="bg-gray-600 flex-1 flex flex-col">
+      <div className="bg-gray-700 flex-1 flex flex-col">
         <div className="px-3 h-12 flex items-center shadow-md">Generals</div>
         <div className="px-3 flex-1 space-y-4 overflow-y-scroll scroll-thin">
           {[...Array(40)].map((_, i) => (
